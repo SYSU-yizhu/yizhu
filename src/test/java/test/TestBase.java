6c2ecc7f3e22ce9ac5f508c82534730c45b9c436
YifengWong <395049219@qq.com>
2017-06-02 00:13:16 +0800

    add spring profile

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -1,12 +1,14 @@
 package test;
 
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration({"file:src/main/webapp/WEB-INF/test-yizhu-servlet.xml"})
+@ContextConfiguration({"file:src/main/webapp/WEB-INF/yizhu-servlet.xml"})
+@ActiveProfiles(profiles = "production")
 public abstract class TestBase {
 }
